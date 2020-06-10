081259e2623fa56d539cbb2c299b43d42e37636a
yubaoliu <yubaoliu89@gmail.com>
2020-06-10 18:08:27 +0900

    Add docker, fixed rviz cannot start error

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "entrypoint"
+
 # setup ros environment
 source "/opt/ros/kinetic/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
 
+apt update
 
 echo "Compile ros project"
 cd /root/catkin_ws
-rosdep install -y rgbdslam
-catkin_make
+rosdep install -y rgbdslam --skip-keys=\"nvidia-cuda-dev nvidia-cuda\"
+catkin_make -j2
 
 exec "$@"
